25a60432f7d9152a8bc8bc9d80a61f36d102462f
Mel Bather <mel.bather@gmail.com>
2025-08-27 16:37:31 -0700

    start skeleton

--- bigfoot-app/app.R
+++ bigfoot-app/app.R
@@ -1,2 +1,24 @@
 # Read in modules
 source("global.R")
+
+ui <- fluidPage(
+  tags$head(
+    includeCSS("www/style.css")
+  ),
+  # Navigation
+  tabBox(
+    width = 12,
+    id = "primary_box",
+    tabPanel(
+      value = "sightings",
+      h3(id = "nav_title", "Sightings"),
+      sightings_ui(id = "sightings")
+    )
+  )
+)
+
+server <- function(input, output) {
+  sightings_server(id = "sightings", parent = session)
+}
+
+shinyApp(ui = ui, server = server)
